e49a8dbfc6cdae015d2d0cc6f897c41acb9e5aa3
chen_null <abc82766@gmail.com>
2022-11-05 20:15:49 +0800

    Move index to normal path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: 'public/index.html'
-        }
-    },
     server: {
         open: '/index.html'
     }
